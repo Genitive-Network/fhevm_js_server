03129ea07ca2c7839f1318d5d38f4973d0d60d1d
Gx <475444812@qq.com>
2024-07-03 18:00:22 +0800

    test

--- src/test.js
+++ src/test.js
@@ -1,17 +1,5 @@
 import {init} from "./fhevm_instance.js";
-import {ADMINISTRATOR_PRIVATE_KEY} from "./config.js";
 import {getInstance} from "./fhevm_instance.js";
-
-// console.log(ADMINISTRATOR_PRIVATE_KEY)
-//
-//
-// console.log(Number.MIN_SAFE_INTEGER)
-// console.log(Number.MAX_SAFE_INTEGER)
-//
-// const myBigInt = BigInt(10);  // `10n` also works
-// const myNumber = Number(myBigInt);
-//
-// console.log(myNumber)
 
 function buf2hex(buffer) { // buffer is an ArrayBuffer
     return [...new Uint8Array(buffer)]
@@ -23,7 +11,7 @@ function buf2hex(buffer) { // buffer is an ArrayBuffer
 await init()
 
 // let encryptedData = getInstance().encrypt64(99);
-let encryptedData = getInstance().encryptAddress("0xDf5410EeCeF97093BE7Eb76183b692F155421c66");
+let encryptedData = getInstance().encryptAddress("0xDbb6F44FA2bDb55c45A4B4119C163f9c42Ac5630");
 console.log(encryptedData)
 let x = buf2hex(encryptedData);
 console.log(x)
